38419145afa481ecbc292a5f8a0ed9ca17246be1
iLLoReal <olivier.laffon.dev@gmail.com>
2023-05-22 18:00:00 +0200

    lint

--- app/routes/factures._index.tsx
+++ app/routes/factures._index.tsx
@@ -5,15 +5,15 @@ import { isUserLoggedIn } from "~/helpers/session";
 import { getSession } from "~/session";
 import { bill } from "~/types/global";
 
-export async function loader({request} : LoaderArgs) {
+export async function loader({ request }: LoaderArgs) {
     const loggedIn = await isUserLoggedIn(request);
     if (!loggedIn) {
         return redirect('/');
-    } 
+    }
     return (json(await getFactures()));
 }
 export default function FacturesIndexRoute() {
-    const  bills = useLoaderData<typeof loader>();
+    const bills = useLoaderData<typeof loader>();
 
     return (
         <main>
